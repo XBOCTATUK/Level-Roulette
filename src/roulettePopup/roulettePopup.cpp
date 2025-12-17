552e9541de7d55148f7947849043b148fb6d506a
xboct <xboct@XBOCT>
2025-12-17 18:07:55 +0700

    for mobile x3 please

--- src/roulettePopup/roulettePopup.cpp
+++ src/roulettePopup/roulettePopup.cpp
@@ -7,6 +7,7 @@ namespace geode::prelude {
 	std::vector<int> g_usedWorstLevels;
 	std::vector<int> g_lvls;
 	LevelData g_currentLvl;
+	LevelData g_pastCurrentLvl;
 	std::unordered_map<std::string, LevelData> g_levelData;
 	std::unordered_map<std::string, ccColor3B> g_quadrantColors = {
 		{"Auto", {234, 173, 86}},
@@ -42,18 +43,17 @@ namespace geode::prelude {
 		{"Hard", "difficulty_03_btn_001.png"},
 		{"Harder", "difficulty_04_btn_001.png"},
 		{"Insane", "difficulty_05_btn_001.png"},
-		{"EasyDemon", "difficulty_07_btn_001.png"},
-		{"MediumDemon", "difficulty_08_btn_001.png"},
-		{"HardDemon", "difficulty_06_btn_001.png"},
-		{"InsaneDemon", "difficulty_09_btn_001.png"},
-		{"ExtremeDemon", "difficulty_10_btn_001.png"}
+		{"EasyDemon", "difficulty_07_btn2_001.png"},
+		{"MediumDemon", "difficulty_08_btn2_001.png"},
+		{"HardDemon", "difficulty_06_btn2_001.png"},
+		{"InsaneDemon", "difficulty_09_btn2_001.png"},
+		{"ExtremeDemon", "difficulty_10_btn2_001.png"}
 	};
 
 	int g_spinsCount = 0;
 	int g_skipsCount = Mod::get()->getSettingValue<int>("skips-count");;
 	int g_requirePercent = 1;
 	int g_currentPercent = 0;
-
 	bool g_afterSpin = true;
 
 	RoulettePopup* g_popup = nullptr;
@@ -108,10 +108,12 @@ bool RoulettePopup::setup() {
 	auto spinSpr = ButtonSprite::create("Start");
 	m_spinBtn = CCMenuItemExt::createSpriteExtra(spinSpr, [this](auto) {
 		if (!g_currentLvl.id.empty()) {
-			g_afterSpin = false;
+			g_spinsCount -= 1;
 			afterSpinOnPopup();
 		}
-		else RouletteLayer::create()->show();
+		else {
+			RouletteLayer::create()->show();
+		}
 	});
 	m_spinBtn->setPosition({ 160.0f, 110.0f });
 	m_spinBtn->setScale(0.8f);
@@ -125,7 +127,6 @@ bool RouletteLayer::setup() {
 	auto winSize = CCDirector::get()->getWinSize();
 	m_bgSprite->setVisible(false);
 	m_closeBtn->setVisible(false);
-	if (g_currentPercent >= g_requirePercent) g_requirePercent += 1;
 
 	m_rouletteWheel = CCMenu::create();
 	m_rouletteWheel->setContentSize({ 0.0f, 0.0f });
@@ -302,7 +303,6 @@ void RouletteLayer::readLevelData() {
 
 void RouletteLayer::spin() {
 	static std::mt19937 mt(std::random_device{}());
-	g_spinsCount += 1;
 
 	if (g_worstLevels.size() >= 4) {
 		for (int i = 0; i < 4; i++) {
@@ -321,7 +321,7 @@ void RouletteLayer::spin() {
 	int levelIndex = 0;
 	for (int i = 0; i < 4; i++) {
 		if (deltaAngle - (90.0f * i) < 90.0f) {
-			g_currentLvl = g_levelData[std::to_string(g_lvls[i])];
+			g_pastCurrentLvl = g_levelData[std::to_string(g_lvls[i])];
 
 			g_worstLevels.erase(std::find(g_worstLevels.begin(), g_worstLevels.end(), g_lvls[i]));
 			g_usedWorstLevels.push_back(g_lvls[i]);
@@ -426,9 +426,11 @@ void RouletteLayer::afterSpinOnLayer() {
 }
 
 void RoulettePopup::afterSpinOnPopup() {
-	if (g_spinsCount == 0) {
+	g_spinsCount += 1;
+	g_currentLvl = g_pastCurrentLvl;
+	if (g_spinsCount == 1) {
 		m_spinBtn->setVisible(false);
-		m_spinsCount->setString("Number of spins: 0");
+		m_spinsCount->setString("Number of spins: 1");
 
 		m_resetBtn = CCMenuItemExt::createSpriteExtraWithFrameName("GJ_updateBtn_001.png", 1.0f, [this](auto) {
 			resetRoulette();
@@ -439,19 +441,23 @@ void RoulettePopup::afterSpinOnPopup() {
 
 		m_diffSpr = CCSprite::createWithSpriteFrameName(g_spriteNames1[g_currentLvl.diff].c_str());
 		m_diffSpr->setPosition({ 75.0f, 105.0f });
+		m_diffSpr->setZOrder(1);
 
 		m_levelName = CCLabelBMFont::create(g_currentLvl.name.c_str(), "bigFont.fnt");
 		if (g_currentLvl.name.size() > 12) m_levelName->setScale(0.5f);
 		else m_levelName->setScale(0.7f);
 		m_levelName->setPosition({ 160.0f, 170.0f });
+		m_levelName->setZOrder(1);
 
 		m_dislikeSpr = CCSprite::createWithSpriteFrameName("GJ_dislikesIcon_001.png");
 		m_dislikeSpr->setPosition({ 224.75f + m_dislikeSpr->getContentWidth() / 2.0f, 105.0f });
 		m_dislikeSpr->setScale(0.75f);
+		m_dislikeSpr->setZOrder(1);
 
 		m_levelDislikes = CCLabelBMFont::create(std::to_string(g_currentLvl.likes).c_str(), "bigFont.fnt");
 		m_levelDislikes->setScale(0.35f);
 		m_levelDislikes->setPosition({ m_dislikeSpr->getPositionX() + m_dislikeSpr->getContentWidth() / 2.0f + m_levelDislikes->getContentWidth() * m_levelDislikes->getScale() / 2.0f, 105.0f });
+		m_levelDislikes->setZOrder(1);
 
 		m_playBtn = CCMenuItemExt::createSpriteExtraWithFrameName("GJ_playBtn2_001.png", 0.8f, [this](auto) {
 			auto searchObj = GJSearchObject::create(SearchType::Search, g_currentLvl.id);
@@ -467,10 +473,12 @@ void RoulettePopup::afterSpinOnPopup() {
 		m_requirePercentBG->setContentSize({ 90.0f, 60.0f });
 		m_requirePercentBG->setScale(0.4f);
 		m_requirePercentBG->setPosition({ 160.0f, 40.0f });
+		m_requirePercentBG->setZOrder(1);
 
 		m_requirePercent = CCLabelBMFont::create(fmt::format("{}%", g_requirePercent).c_str(), "bigFont.fnt");
 		m_requirePercent->setScale(0.4f);
 		m_requirePercent->setPosition({ 160.0f, 40.0f });
+		m_requirePercent->setZOrder(1);
 
 		auto skipSpr = ButtonSprite::create("Skip", "bigFont.fnt", "GJ_button_03.png");
 		m_skipBtn = CCMenuItemExt::createSpriteExtra(skipSpr, [this](auto) {
@@ -482,7 +490,7 @@ void RoulettePopup::afterSpinOnPopup() {
 			}
 			else {
 				g_skipsCount -= 1;
-				if (g_currentPercent < g_requirePercent) g_requirePercent += 1;
+				g_requirePercent += 1;
 				RouletteLayer::create()->show();
 			}
 		});
@@ -493,16 +501,18 @@ void RoulettePopup::afterSpinOnPopup() {
 		m_skipsCount = CCLabelBMFont::create(fmt::format("skips: {}", g_skipsCount).c_str(), "goldFont.fnt");
 		m_skipsCount->setScale(0.4f);
 		m_skipsCount->setPosition({ 160.0f, 64.0f });
+		m_skipsCount->setZOrder(1);
 
 		auto nextSpr = ButtonSprite::create("Next", "bigFont.fnt", "GJ_button_01.png");
 		m_nextBtn = CCMenuItemExt::createSpriteExtra(nextSpr, [this](auto) {
-			if (g_requirePercent == 100) {
+			if (g_requirePercent > 100) {
 				FLAlertLayer::create("Congrats", "You passed the roulette. =3", "Hurray!")->show();
 			}
 			else if (g_currentPercent < g_requirePercent) {
 				FLAlertLayer::create("Nope", "You haven't reached the required percentage. =3", "Ok")->show();
 			}
 			else {
+				g_requirePercent += 1;
 				RouletteLayer::create()->show();
 			}
 		});
@@ -606,7 +616,7 @@ void RoulettePopup::afterSpinOnPopup() {
 				}
 				else {
 					g_skipsCount -= 1;
-					if (g_currentPercent < g_requirePercent) g_requirePercent += 1;
+					g_requirePercent += 1;
 					RouletteLayer::create()->show();
 				}
 				});
@@ -625,6 +635,7 @@ void RoulettePopup::afterSpinOnPopup() {
 					FLAlertLayer::create("Nope", "You haven't reached the required percentage. =3", "Ok")->show();
 				}
 				else {
+					g_requirePercent += 1;
 					RouletteLayer::create()->show();
 				}
 				});
@@ -658,10 +669,7 @@ void RoulettePopup::resetRoulette() {
 			g_worstLevels.insert(g_worstLevels.end(), g_usedWorstLevels.begin(), g_usedWorstLevels.end());
 			g_usedWorstLevels.clear();
 			g_lvls.clear();
-			g_currentLvl.name = "";
-			g_currentLvl.diff = "";
-			g_currentLvl.id = "";
-			g_currentLvl.likes = 0;
+			g_currentLvl = LevelData();
 
 			g_spinsCount = 0;
 			g_skipsCount = Mod::get()->getSettingValue<int>("skips-count");;
